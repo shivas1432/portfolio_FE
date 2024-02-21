79dda74b9d10406046eb15370c8d2da9d8c7eea7
shivas1432 <shivashanker7337@gmail.com>
2024-02-21 17:39:00 +0000

    improved responsive design

---
+++ src/components/Learning8393.jsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+// Learning React basics - January 2024
+const Learning8393 = () => {
+    return (
+        <div className="learning-component">
+            <h2>Learning Component 8393</h2>
+            <p>Created while learning React fundamentals</p>
+            <p>Date: 2024-01-27</p>
+            <div>
+                <button>Click me</button>
+                <p>Still learning event handling</p>
+            </div>
+        </div>
+    );
+};
+
+export default Learning8393;
